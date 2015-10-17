66b3eb70665b7fae9d66a29d819cb10217a2d2b0
jdesesquelles <jdesesquelles@gmail.com>
2015-10-17 20:40:22 +0200

    Feat
    GCE pull jokes from the Java library

---
+++ jokebackend/src/main/java/com/udacity/gradle/builditbigger/jokebackend/MyEndpoint.java
@@ -0,0 +1,53 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.udacity.gradle.builditbigger.jokebackend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import javax.inject.Named;
+
+import com.udacity.gradle.javajokes.Joker;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "jokebackend.builditbigger.gradle.udacity.com",
+                ownerName = "jokebackend.builditbigger.gradle.udacity.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+        response.setData("Hi, " + name);
+
+        return response;
+    }
+
+    @ApiMethod(name = "tellJoke")
+    public MyBean tellJoke() {
+        Joker myJoke = new Joker();
+//        myData = myJoke.getJoke();
+        MyBean response = new MyBean();
+        response.setData(myJoke.getJoke());
+
+        return response;
+    }
+
+
+}
